17d70677560ee02cf40dbc060fd858e3052c29ff
Rostyslav.Kibkalo <rostislav.kibkalo@gmail.com>
2022-05-30 18:07:11 +0300

    123

--- public/index.php
+++ public/index.php
@@ -2,12 +2,12 @@
 
 ini_set('display_errors', '1');
 
-include __DIR__ ."/../vendor/autoload.php";
+include __DIR__ . "/../vendor/autoload.php";
 
 $component = new \mvc\app\Components\Routing\RouterFactory();
 
 $router = $component->createComponent();
 
 $action = $router->route($_SERVER['REQUEST_URI']);
-//var_dump($action());
+
 $action();
